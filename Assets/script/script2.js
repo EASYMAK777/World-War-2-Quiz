dbabec473e129608407f7a772a44eef42a4aa328
JuanFlores <juanflz7777@gmail.com>
2022-08-05 10:15:24 -0700

    created timer element, button starts timer, console log game over when timer ends

--- Assets/script/script2.js
+++ Assets/script/script2.js
@@ -1,14 +1,27 @@
+// //////////////////////////////////////////EVENT LISTENER THAT DISPLAYS TIMER IN DIV/////////////////////////////////////////
+
 var startButton = document.querySelector("#start");
-var TimeLeft = 5;
-TimerEl = document.querySelector("#timerEl");
+var timeLeft = 5;
+timerEl = document.querySelector("#timerEl");
 
 function setTimerText() {
-  TimerEl.textContent = TimeLeft;
+  var timerInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft + "seconds till game ends";
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      //   endGame();
+      console.log("game over");
+    }
+  }, 1000);
 }
 
 startButton.addEventListener("click", function () {
-  console.log(TimeLeft);
+  console.log(timeLeft);
 
-  if (TimeLeft < 5) TimeLeft--;
+  if (timeLeft < 5) timeLeft--;
   setTimerText();
 });
+
+///////////////////////////////////////TIMER THAT COUNTS DOWN WHEN BUTTON IS CLICKED AND TIME IS DISPLAYED
