7c19d4f1cfe9cc1731f89461790e316d4f143e93
JuanFlores <juanflz7777@gmail.com>
2022-08-25 09:23:41 -0700

    reworking a function

--- Assets/script/script2.js
+++ Assets/script/script2.js
@@ -21,9 +21,9 @@ function setTimerText() {
 startButton.addEventListener("click", function () {
   console.log("Game Start");
   //   //////////////////////////////////////////////////////DISPLAYS QUESTION///////////////////////////////////////
-  displayQuestion();
   if (timeLeft < 5) timeLeft--;
   setTimerText();
+  displayQuestion();
 });
 
 // Ends game when timer expires
@@ -34,31 +34,5 @@ function endGame() {
   console.log("Game End");
 }
 
-///////////////////////////////////////QUESTION DIV///////////////////////////////////////////////////
-//////////////////////////////THIS DIV SHOULD DISPLAY QUESTIONS, AND ANSWER CHOICES, WHEN USER CLICKS CHOICE THE GAME LOGIC SHOULD DETERMINE IF THE /////////
-////////////////////////////(CONT) USER CHOICE WAS CORRECT OR INCORRECT, SWITCH TO NEXT QUESTION//////////////////////
-
-// function that displays question when start button is clicked
-
-let questionList = [
-  {
-    question: "What year did world war 2 start?",
-    choiceA: "1999",
-    choiceB: "2022",
-    choiceC: "1933",
-    choiceD: "1776",
-    correct: "C",
-  },
-];
-function displayQuestion() {}
-//Console log first question
-console.log(questionList[0].question);
-////////////////////////////////////////////////////SCORE BOARD//////////////////////////////////////////////////////////
-//////////////////////////////ADDS TO SCOREBOARD OR TAKES AWAY FROM TIME DEPENDING ON USER INPUT////////////////////////////////
 var playerScore = 0;
 var scoreBoard = "";
-
-// for (var i = question; i < questionList.question1; i++) {
-//   var question = questionList.question1;
-//   var answers = questionList.answerChoices;
-// }
