9a1f369e8163223514ad8c3b207eb263200c0a70
JuanFlores <juanflz7777@gmail.com>
2022-09-18 00:42:50 -0700

    get El by id start button, console log start button

---
+++ Assets/script/script2.js
@@ -0,0 +1,8 @@
+//get element by id startBtn
+var startBtn = document.getElementById("startBtn");
+
+//console.log(startBtn) when button is clicked
+
+startBtn.addEventListener("click", function () {
+  console.log("button clicked");
+});
