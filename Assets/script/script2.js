36bff7582f54bbff0548721e33689e7deb510776
JuanFlores <juanflz7777@gmail.com>
2022-09-18 01:16:54 -0700

    displaying scoreboard on top right of doc

--- Assets/script/script2.js
+++ Assets/script/script2.js
@@ -1,8 +1,11 @@
 //get timerEl from html
 var timerEl = document.getElementById("timeLeft");
+var scoreEl = document.getElementById("score");
 
 //set timer to 75 seconds
 var timeLeft = 75;
+var score = 0;
 
 //display time in timeLeft element
 timerEl.textContent = timeLeft;
+scoreEl.textContent = score;
